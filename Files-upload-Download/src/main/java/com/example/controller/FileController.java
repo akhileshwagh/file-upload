ee2b240c3f29881530e69cde820c29388410246c
akhileshwagh <akhimech05@gmail.com>
2025-06-27 19:59:34 +0530

    file-upload with data implemented

--- Files-upload-Download/src/main/java/com/example/controller/FileController.java
+++ Files-upload-Download/src/main/java/com/example/controller/FileController.java
@@ -2,6 +2,8 @@ package com.example.controller;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import org.apache.commons.io.FilenameUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +11,22 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.model.Product;
 import com.example.service.FileService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Slf4j
 @RestController
 public class FileController {
 
@@ -47,18 +55,16 @@ public class FileController {
 
 		try {
 			byte[] downloadFile = fileService.downloadFile(file);
-			
+
 			String contentType = getContentType(file);
-			
-			HttpHeaders headers=new HttpHeaders();
+
+			HttpHeaders headers = new HttpHeaders();
 			headers.setContentType(MediaType.parseMediaType(contentType));
-			//headers.setContentLength(file.length());
+			// headers.setContentLength(file.length());
 			headers.setContentDispositionFormData("attachment", file);
-			
+
 			return ResponseEntity.ok().headers(headers).body(downloadFile);
-			
-			
-			
+
 		} catch (FileNotFoundException e) {
 			return new ResponseEntity<>("file not found", HttpStatus.NOT_FOUND);
 
@@ -66,14 +72,13 @@ public class FileController {
 			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
 		}
 
-		
 	}
 
 	public String getContentType(String fileName) {
 
 		String extension = FilenameUtils.getExtension(fileName);
 		switch (extension) {
-		
+
 		case "pdf":
 			return "applcation/pdf";
 		case "xlsx":
@@ -86,6 +91,51 @@ public class FileController {
 			return "application/octet-stream";
 
 		}
+	}
+
+	@PostMapping("/upload-data")
+	public ResponseEntity<?> uploadFileWithData(@RequestParam String product, @RequestParam MultipartFile file) {
+//		log.info("Product:{}", product);
+//		log.info("file:{}", file);
+
+		List<String> extension = Arrays.asList("jpeg", "jpg", "png");
+		if (file.isEmpty()) {
+			return new ResponseEntity<>("please upload file ", HttpStatus.BAD_REQUEST);
+		} else {
+			String originalFilename = file.getOriginalFilename();
+			String fileExtension = FilenameUtils.getExtension(originalFilename);
+			boolean contains = extension.contains(fileExtension);
+			if (!contains) {
+				return new ResponseEntity<>("please upload image file ", HttpStatus.BAD_REQUEST);
+
+			}
+		}
+		try {
+			String filename = fileService.uploadFileWithData(file);
+			if (StringUtils.hasText(filename)) {
+
+				ObjectMapper objectMapper = new ObjectMapper();
+				Product productObj = objectMapper.readValue(product, Product.class);
+
+				// String filename = file.getOriginalFilename();
+				productObj.setImageName(filename);
+
+				Boolean saveProduct = fileService.saveProduct(productObj);
+				if (saveProduct) {
+					return new ResponseEntity<>("file and image uploaded successfully", HttpStatus.CREATED);
+				} else {
+					return new ResponseEntity<>("file uploaded but product not saved",
+							HttpStatus.INTERNAL_SERVER_ERROR);
+				}
+
+			} else {
+				return new ResponseEntity<>("file uploaded failed", HttpStatus.INTERNAL_SERVER_ERROR);
+			}
+
+		} catch (Exception e) {
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
 	}
 
 }
--- Files-upload-Download/src/main/java/com/example/serviceImpl/FileServiceImpl.java
+++ Files-upload-Download/src/main/java/com/example/serviceImpl/FileServiceImpl.java
@@ -7,12 +7,18 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.UUID;
 
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StreamUtils;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.Repository.ProductRepo;
+import com.example.model.Product;
 import com.example.service.FileService;
 
 @Service
@@ -20,6 +26,9 @@ public class FileServiceImpl implements FileService {
 
 	@Value("${file.upload.path}")
 	private String uploadPath;
+
+	@Autowired
+	private ProductRepo productRepo;
 
 	@Override
 	public Boolean uploadFile(MultipartFile file) throws IOException {
@@ -46,7 +55,7 @@ public class FileServiceImpl implements FileService {
 			InputStream ios = new FileInputStream(fullPath);
 
 			return StreamUtils.copyToByteArray(ios);
-			
+
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 			throw e;
@@ -55,7 +64,42 @@ public class FileServiceImpl implements FileService {
 			throw e;
 		}
 
-	
 	}
+
+	@Override
+	public Boolean saveProduct(Product product) {
+		Product save = productRepo.save(product);
+		if (!ObjectUtils.isEmpty(save)) {
+			return true;
+		}
+		return false;
+
+	}
+
+	@Override
+	public String uploadFileWithData(MultipartFile file) throws IOException {
+		
+		String fileName = file.getOriginalFilename();
+
+		File saveFile = new File(uploadPath);
+		
+		String rndString = UUID.randomUUID().toString();
+		String removeExtension = FilenameUtils.removeExtension(fileName);
+		String extension = FilenameUtils.getExtension(fileName);
+		fileName=removeExtension+"_"+rndString+"."+extension;
+		
+		
+		if (!saveFile.exists()) {
+			saveFile.mkdir();
+		}
+		String storePath = uploadPath.concat(fileName);
+		long upload = Files.copy(file.getInputStream(), Paths.get(storePath));
+		if (upload != 0) {
+			return fileName;
+		}
+
+		return null;
+	}
+	
 
 }
